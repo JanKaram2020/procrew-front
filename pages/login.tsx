455ebdb0aab91c25d0a8ebee6eb71badedd75767
Jan Karam <65977048+JanKaram2020@users.noreply.github.com>
2021-11-07 20:10:18 +0200

    fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,8 +5,10 @@ import { validationSchema } from 'users/signUpSchema';
 import { useState } from 'react';
 import NextLink from 'next/link';
 import { AiFillGithub } from 'react-icons/ai';
+import { useRouter } from 'next/router';
 
 const LoginPage = () => {
+    const router = useRouter();
     const [{ isLoading, isSuccess, isError }, setState] = useState<{
         isLoading: boolean;
         isSuccess: boolean | null;
@@ -58,16 +60,25 @@ const LoginPage = () => {
                 signIn('credentials', {
                     email: formik.values.email,
                     password: formik.values.password,
-                    redirect: true,
-                    callbackUrl: 'https://procrew-front.vercel.app/',
-                }).then((error) => {
-                    console.log(error);
-                    setState({
-                        isLoading: false,
-                        isSuccess: false,
-                        isError: true,
+                    redirect: false,
+                })
+                    .then((log) => {
+                        console.log(log);
+                        setState({
+                            isLoading: false,
+                            isSuccess: true,
+                            isError: false,
+                        });
+                        router.push('/');
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        setState({
+                            isLoading: false,
+                            isSuccess: false,
+                            isError: true,
+                        });
                     });
-                });
             }}
         >
             <Heading>Login</Heading>
